67dcc0f09b9b00774bd7ea725dec32f817f22855
bledoux <bledoux@france24.com>
2018-11-21 15:13:06 +0100

    added Flask config, corrected some Poem, added some UT for Poem

--- reporter/api.py
+++ reporter/api.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python 
 # coding=utf8
 
+# imports
 from flask import Flask
 from reporter import header
-app = Flask(__name__)
+from reporter.poem import Poem
 
+# init Flask
+app = Flask(__name__)
+app.config.from_pyfile('../api.cfg')
+
+# init Poem
+poem = Poem(url=app.config['POEM_URL'], selector=app.config['POEM_SELECTOR'])
+poem._fetch_poem()
+
+
+# HTTP routing
 @app.route("/")
 def hello():
-    return header.create_header('data/authors.json', 'Paris XII')
+    return header.create_header(app.config['AUTHOR_PATH'], 'Paris XII')
+
--- reporter/poem.py
+++ reporter/poem.py
@@ -8,12 +8,14 @@ class Poem:
 
     def __init__(self,
             url="https://fr.wikisource.org/wiki/Po%C3%A9sies_(Rimbaud)/%C3%A9d._Vanier,_1895/Le_Bateau_ivre",
+            selector="div.poem",
             proxies={'http': None, 'https': None}):
         self.proxy = proxies
         self.url = url
+        self.selector = selector
         self.session = HTMLSession()
-        self.poem = []
+        self.elements = []
 
     def _fetch_poem(self):
         r = self.session.get(self.url, proxies=self.proxy)
-        self.poem = [poem.text for poem in r.html.find("div.poem")]
+        self.elements = [p.text for p in r.html.find(self.selector)]
