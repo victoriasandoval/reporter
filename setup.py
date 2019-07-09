8a2c8e4b7a9a4b9baff2fd39aafe12e6872f529e
Guillaume Gay <guillaume@damcb.com>
2019-07-09 17:00:08 +0200

    Cleaned up before session start

--- setup.py
+++ setup.py
@@ -1,25 +1,14 @@
-import os
-import sys
-import subprocess
-
 from setuptools import setup
 from setuptools import find_packages
 
 
-DISTNAME = 'reporter'
+DISTNAME = "reporter"
 
-MAJOR               = 0
-MINOR               = 0
-MICRO               = 1
-ISRELEASED          = False
-VERSION             = f'{MAJOR}.{MINOR}.{MICRO}'
+MAJOR = 0
+MINOR = 0
+MICRO = 1
+ISRELEASED = False
+VERSION = f"{MAJOR}.{MINOR}.{MICRO}"
 
 
-if __name__ == "__main__":
-
-    setup(
-        name=DISTNAME,
-        version=VERSION,
-        packages=find_packages(),
-        zip_safe=False
-    )
+setup(name=DISTNAME, version=VERSION, packages=find_packages(), zip_safe=False)
